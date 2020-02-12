7556d8995064a4e59fafdd64ab54be25ce155d51
bidasan@apac.corpdir.net <bidasan@apac.corpdir.net>
2020-02-12 23:09:17 +0530

    Submitting the code for final code

---
+++ src/ChildReact.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import App from './App';
+
+class ChildReact extends React.Component {
+    render() {
+        return  <App />;
+    }
+}
+
+export default ChildReact;
